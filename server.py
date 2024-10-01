3a95219f15083954551f7eee7b8a74a2c266ce9c
MattD201 <mdelia@wwinfotech.com>
2024-10-01 14:30:11 -0400

    fixed returns

--- server.py
+++ server.py
@@ -8,13 +8,13 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def emotion_detect():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
-    # Pass the text to the sentiment_analyzer function and store the response
+    # Pass the text to the emotion detector function and store the response
     response = emotion_detector(text_to_analyze)
     # Return a formatted string with the emotion detection scores
-    return "For the given statement, the system response is {} with a score of {}.".format(label.split('_')[1], score)
+    return response
 
 @app.route("/")
 def render_index_page():
